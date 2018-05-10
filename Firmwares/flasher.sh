d38667af2b6256d1c03a359dd3d0829f8ec5f6c9
Charlton Trezevant <ct@ctis.me>
2018-05-09 22:51:24 -0400

    relative paths in flasher.sh

--- Firmwares/flasher.sh
+++ Firmwares/flasher.sh
@@ -3,13 +3,13 @@
 if [ "$1" == "factory" ]; then
   echo "Flashing Factory firmware to ATMega16u2..."
   dfu-programmer atmega16u2 erase
-  dfu-programmer atmega16u2 flash ./Firmwares/atmega16u2_UNOr3_default_firmware.hex
+  dfu-programmer atmega16u2 flash ./atmega16u2_UNOr3_default_firmware.hex
   echo "Done!"
 fi
 
 if [ "$1" == "midi" ]; then
   echo "Flashing MIDI firmware to ATMega16u2..."
   dfu-programmer atmega16u2 erase
-  dfu-programmer atmega16u2 flash ./Firmwares/arduino_midi.hex
+  dfu-programmer atmega16u2 flash ./arduino_midi.hex
   echo "Done!"
 fi
