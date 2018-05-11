e006e3a1ef91f3eb2f4dca098630726d83080984
Charlton Trezevant <ct@ctis.me>
2018-05-11 15:10:19 -0400

    Add initial work on glide function, as well as documenatation and refactoring

--- arduino_main.c
+++ arduino_main.c
@@ -3,11 +3,57 @@
 // github.com/ddiakopoulos/hiduino to provide MIDI functionality via USB
 #include "MIDI.h"
 
-byte lastNoteOn;
-int notePlaying;
-float pitchBendMultiplier;
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+/*
+  State keeps track of the synthesizer's current state in a
+  globally accessable struct.
+  
+  Values are updated on an as-needed basis by handler functions according to messages
+  recieved via MIDI.
+  
+  Values are read in cases where the functionality of some handlers is
+  affected by the synthesizer's current state.
+  
+  For example:
+  
+  noteOn:
+    On any noteOn message, the lastNotePlayed and lastNoteCV values are updated to match the
+    current note. To reflect thefact that a note is being held down, the noteIsPlaying value will be set to 1.
+    Finally, the noteOn handler will write the latest value to the DAC, taking into account any pitch bending
+    being applied.
+  
+  noteOff:
+    noteOff should will apply only to the most recent note that was played. Some implementations I've seen
+    do not perform this check, so the Gakken will stop playing completely when any noteOff message comes in
+    (which is not what we want).
+    
+    To enforce this, the noteOff message handler will check against the current state: A noteOff for C3 would cause C3 to stop
+    playing if and only if the value of lastNotePlayed is C3, and a note is currently being played (i.e noteIsPlaying = 1).
+    In practice, this works like so: if you were to play C, then tap E (while still holding C), and release C (while still
+    holding E), E would remain playing.
+  
+  Pitch Bend:
+    The pitchbend handler will always react to incoming pitchbend messages, but the result will only be applied in certain cases.
+    This handler performs a sanity check on the calculated pitchBendMultiplier against the current CV value, to ensure that invalid
+    pitchbend multipliers are not generated for the current note.
+    
+    Assuming the check passes, the State's pitchBendMultiplier will be updated with the new value. Finally, the handler will write
+    a new, pitch-bent CV value to the DAC only if a note is playing. This is what allows you to apply pitch bend in realtime to
+    a note you're currently holding.
+  
+  Modulation/Midi CC:
+    The Midi Control Change (CC) messages handler acts as a dispatch to handlers for various CC message types.
+    
+*/
+struct State {
+  byte lastNotePlayed;
+  uint16_t lastNoteCV;
+  int noteIsPlaying;
+  float pitchBendMultiplier;
+  float noteGlideRate;
+};
+
+struct State state;
 
 // Implements SPI for use with the MCP4921 DAC, which
 // sends CV to the Gakken
@@ -15,6 +61,8 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 #define DATAIN 12 //MISO - not used, but part of builtin SPI
 #define SPICLOCK  13 //sck
 #define SLAVESELECT 10 //ss
+
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 void gakken_write_value(uint16_t sample) {
   uint8_t dacSPI0 = 0;
@@ -61,31 +109,58 @@ uint16_t note_to_cv(byte pitch) {
   return 1210 - diff;
 }
 
-void gakken_noteOn(byte channel, byte pitch, byte velocity) {
-  lastNoteOn = pitch;
-  notePlaying = 1;
+void handle_noteOn(byte channel, byte pitch, byte velocity) {
+  state.lastNotePlayed = pitch;
+  state.noteIsPlaying = 1;
   uint16_t cv_value = note_to_cv(pitch);
-  gakken_write_value(cv_value * pitchBendMultiplier);
+  
+  if(state.noteGlideRate > 0)
+    glide_to(cv_value);
+  
+  gakken_write_value(cv_value * state.pitchBendMultiplier);
 }
 
-void gakken_noteOff(byte channel, byte pitch, byte velocity) {
-  if(pitch == lastNoteOn && notePlaying == 1){
-      notePlaying = 0;
-      gakken_write_value(0);
-  }
+
+void handle_noteOff(byte channel, byte pitch, byte velocity) {
+  
+  // We should only handle not OFF messages if the message is for the note that's
+  // currently being held, and additionally only if a note is currently playing.
+  if(pitch != state.lastNotePlayed || state.noteIsPlaying == 0)
+    return;
+
+  state.noteIsPlaying = 0;
+  gakken_write_value(0);
 }
 
-void gakken_pitchBend(byte channel, int bend) {
-  int current_cv_value = note_to_cv(lastNoteOn);
+void handle_pitchBend(byte channel, int bend) {
+  int current_cv_value = note_to_cv(state.lastNotePlayed);
   float bendAmount = 1 + (((float) bend) / 8190);
   int bentVal = current_cv_value * bendAmount;
 
-  if (bentVal > 0){
-    pitchBendMultiplier = bendAmount;
+  // After bending, the next CV value we're going to write should never
+  // be less than 0.
+  if (bentVal < 0)
+    return;
     
-    if(notePlaying == 1)
-      gakken_write_value(bentVal);
+  state.pitchBendMultiplier = bendAmount;
+    
+  if(state.noteIsPlaying == 1)
+    gakken_write_value(bentVal);
+}
+
+// Recieves all incoming Control Change messages (CC), and dispatches
+// the payloads to their appropriate handlers
+void handle_CC(byte channel, byte number, byte value){
+  
+  switch(number){
+    case 1: // MIDI CC #1 is the Mod Wheel
+      set_glide_parameters(channel, number, value);
+      break;
+      
+    default:
+      return;
   }
+  
 }
 
 void setup()
@@ -97,22 +172,23 @@ void setup()
   pinMode(SPICLOCK,OUTPUT);
   pinMode(SLAVESELECT,OUTPUT);
   digitalWrite(SLAVESELECT,HIGH); //disable device
-  
   SPCR = (1<<SPE)|(1<<MSTR);
   clr=SPSR;
   clr=SPDR;
   
   delay(10);
   
-  lastNoteOn = 0;
-  notePlaying = 0;
-  pitchBendMultiplier = 1;
+  state.lastNotePlayed = 0;
+  state.noteIsPlaying = 0;
+  state.pitchBendMultiplier = 1;
+  state.noteGlideRate = 0;
 
   // MIDI library configuration
   MIDI.begin(MIDI_CHANNEL_OMNI);
-  MIDI.setHandleNoteOn(gakken_noteOn);
-  MIDI.setHandleNoteOff(gakken_noteOff);
-  MIDI.setHandlePitchBend(gakken_pitchBend);
+  MIDI.setHandleNoteOn(handle_noteOn);
+  MIDI.setHandleNoteOff(handle_noteOff);
+  MIDI.setHandlePitchBend(handle_pitchBend);
+  MIDI.setHandleControlChange(handle_CC);
 }
 
 void loop() {
